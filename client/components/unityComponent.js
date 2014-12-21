67a246d9bc015c8382a308803bdbd9f7e9a0e419
neobii <Iwilldevelopcodeforyou@gmail.com>
2014-12-21 00:04:52 -0500

    added unity container

---
+++ client/components/unityComponent.js
@@ -0,0 +1,47 @@
+Template.unityComponent.rendered = function(){
+	var config = {
+				width: 960, 
+				height: 600,
+				params: { enableDebugging:"0" }
+				
+			};
+			var u = new UnityObject2(config);
+
+			$(function() {
+
+				var $missingScreen = $("#unityPlayer").find(".missing");
+				var $brokenScreen = $("#unityPlayer").find(".broken");
+				$missingScreen.hide();
+				$brokenScreen.hide();
+				
+				u.observeProgress(function (progress) {
+					switch(progress.pluginStatus) {
+						case "broken":
+							$brokenScreen.find("a").click(function (e) {
+								e.stopPropagation();
+								e.preventDefault();
+								u.installPlugin();
+								return false;
+							});
+							$brokenScreen.show();
+						break;
+						case "missing":
+							$missingScreen.find("a").click(function (e) {
+								e.stopPropagation();
+								e.preventDefault();
+								u.installPlugin();
+								return false;
+							});
+							$missingScreen.show();
+						break;
+						case "installed":
+							$missingScreen.remove();
+						break;
+						case "first":
+						break;
+					}
+				});
+				u.initPlugin($("#unityPlayer")[0], "meteorBuild.unity3d");
+			});
+		-->
+}
